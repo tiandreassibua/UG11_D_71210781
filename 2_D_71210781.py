1be63609a5235d883324396e70ce13e2d8e241c1
andreas <andreassibua@gmail.com>
2021-11-25 23:58:01 +0700

    UG11

--- 2_D_71210781.py
+++ 2_D_71210781.py
@@ -1,13 +1,24 @@
-input = input("Masukan string : ")
+# input = input("Masukan string : ")
 
-if type(input) == int:
-    print(input,"adalah string")
+# if type(input) == int:
+#     print(input,"adalah string")
   
-else:
-    angka = int(input)
-    if (angka % 2 == 0):
-        res = angka / 2
-        print(res)
-    else:
-        res = (angka + 5) / 2
-        print(res)
+# else:
+#     angka = int(input)
+#     if (angka % 2 == 0):
+#         res = angka / 2
+#         print(res)
+#     else:
+#         res = (angka + 5) / 2
+#         print(res)
+
+word = input("Masukan sebuah kata/kalimat : ")
+char = input("Masukan karakter yang ingin disisipkan : ")
+
+def sisip():
+    x = word.upper()
+    y = word.title()
+    print(char.join(list(x)))
+    print(char.join(y.split()))
+
+sisip()
