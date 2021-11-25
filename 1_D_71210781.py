b269c88a87d8ab7f27f5e18b9e82b65f87cf7689
andreas <andreassibua@gmail.com>
2021-11-26 06:04:07 +0700

    UG11

--- 1_D_71210781.py
+++ 1_D_71210781.py
@@ -1,42 +1,31 @@
 input = input("Masukan string : ")
 
-# if type(input) != str():
-#     angka = int(input)
-#     if (angka % 2 == 0):
-#         res = angka / 2
-#         print(res)
-#     else:
-#         res = (angka + 5) / 2
-#         print(res)
-
-# else:
-#     print(input,"adalah string")
-
-# def reverse(string): 
-#     return string[::-1]
-
-# def numeric(angka):
-#     num = int(angka)
+# def result(input):
+#     # if type(string) == str:
+#     #     print(string[::-1])
+#     num = int(input)
 #     if num % 2 == 0:
-#         res = num / 2
+#         res = input / 2
 #         print(res)
 #     else:
 #         res = (num + 5) / 2
 #         print(res)
+# if type(result()) == int:
+#     print(result())
+# else:
+#     print(input[::-1])
 
-# print(numeric(input))
+def rev(string):
+    res = string[::-1]
+    print(res)
 
-def result(input):
-    # if type(string) == str:
-    #     print(string[::-1])
-    num = int(input)
-    if num % 2 == 0:
-        res = input / 2
-        print(res)
-    else:
-        res = (num + 5) / 2
-        print(res)
-if type(result()) == int:
-    print(result())
+if input is not str:
+    rev(input)
+
 else:
-    print(input[::-1])
+    if input % 2 == 0:
+        print(input / 2)
+    else:
+        print((input + 5) / 2)
+
+# if input in str:
